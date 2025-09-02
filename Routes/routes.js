4d7d6e4a26b863b9d0f6de5a491dc1d1af21394e
Kapil Dagar <kapildagar600@gmail.com>
2025-09-03 02:06:38 +0530

    final commit

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,36 +1,44 @@
 
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 const pool = require('../config/db.js');
-
+// const  upload  = require('../utils/multer.js');
+const cloudinary = require('../utils/cloudinary.js')
+const multer = require('multer')
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'schoolImages/'),
-    filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
-});
+
+
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+
 
 
 
 router.post('/schools', upload.single('image'), async (req, res) => {
     const { name, address, city, state, email } = req.body;
     const contact = Number(req.body.contact); 
-    const image = req.file ? req.file.filename : null;
+    
 
-    if (!name || !address || !city || !state || !contact || !email || !image) {
+    if (!name || !address || !city || !state || !contact || !email || !req.file) {
         return res.status(400).json({ error: "All fields are required and contact must be numeric" });
     }
 
     try {
+        const base64 = `data:${req.file.mimetype};base64,${req.file.buffer.toString('base64')}`;
+        const result = await cloudinary.uploader.upload(base64, {
+      folder: 'schoolImages',
+    });
         await pool.query(
             "INSERT INTO schools (name, address, city, state, contact, image, email) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            [name, address, city, state, contact, image, email]
+            [name, address, city, state, contact, result.secure_url, email]
         );
-        res.json({ message: "School added successfully" });
+        res.json({ message: "School added successfully",
+            imageUrl: result.secure_url
+         });
     } catch (err) {
+        console.log(err)
         res.status(500).json({ error: err.message });
     }
 });
---
+++ utils/multer.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const multer = require('multer')
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
+
+module.exports = upload;
